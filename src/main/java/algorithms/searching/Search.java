f966605900c336e04a3a0c86f909e26f744181aa
Indian-Vendetta <70918181+Indian-Vendetta@users.noreply.github.com>
2023-12-26 13:58:44 +0530

    Update Search.java
    Removed the unused methods

--- src/main/java/algorithms/searching/Search.java
+++ src/main/java/algorithms/searching/Search.java
@@ -2,7 +2,5 @@ package algorithms.searching;
 
 public interface Search {
 
-    boolean checkElementExists(int[] numberArray, int searchElement);
-
     int getSeachElementPosition(int[] numberArray, int searchElement);
 }
